36c3ae404826b76124379923dfd1673ce5da5534
Shakka228 <b_mukhtarbek@kbtu.kz>
2026-03-19 04:53:59 +0500

    Update write_files.py

---
+++ Practice6/file_handling/write_files.py
@@ -0,0 +1,6 @@
+# Create and write to a file
+with open("sample.txt", "w") as f:
+    f.write("Hello, this is the first line.\n")
+    f.write("This is the second line.\n")
+
+print("File created and written successfully.")
